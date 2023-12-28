a9138e2d89e5baa6d003c71de83902bbaa322964
RINKIFU <rinjikafuki@gmail.com>
2023-12-28 10:42:44 +0700

    Upload di Github

---
+++ ReadMe.txt
@@ -0,0 +1,33 @@
+ S C R I P T  O W N  B Y  AZIZX O F C🔭
+
+ - - THANKS TO - -
+ • Allah SWT
+ • MAMA
+ • BAPAK
+ • ADIK
+ • MBAK
+ • NENEK
+ • KAKEK
+ • TANTE
+ • PONAKAN
+ • SEMUA KELUARGA LAH POKOKNYA
+ • Dll
+
+[!] Jangan Lupa Ganti Ownernya Yah Dengan Cara di config.js
+
+ Contact AzizX :  wa.me/62857245372033
+ 
+✧━━━━[ INFO JUALAN ]━━━━✧
+ 
+ ⫹⫺ VPS PANEL
+ ⫹⫺ PT ADMIN PANEL
+ ⫹⫺ ADMIN PANEL
+ ⫹⫺ RESELLER PANEL
+ ⫹⫺ SC MD
+ ⫹⫺ SC BUG
+ ⫹⫺ SC CAMPURAN
+ ⫹⫺ SC CREATE PANEL
+ ⫹⫺ SC CREATE PANEL CAMPUR BUG
+ ⫹⫺ SC HWMODS
+ ⫹⫺ SC SUBDOMAIN×PUSH×CPANEL
+ ⫹⫺ DAN LAIN" LAIN"
---
+++ config.js
@@ -0,0 +1,43 @@
+//EDIT OWNER/ATUR APIKEY DI BAWAH
+
+function _0x2a34(_0x717007,_0x3ea575){const _0x475331=_0x4753();return _0x2a34=function(_0x2a3418,_0x42ccfe){_0x2a3418=_0x2a3418-0x134;let _0x5e9290=_0x475331[_0x2a3418];return _0x5e9290;},_0x2a34(_0x717007,_0x3ea575);}const _0x21f4fc=_0x2a34;(function(_0x26f60c,_0x3708a2){const _0x46656b=_0x2a34,_0x11c0c0=_0x26f60c();while(!![]){try{const _0x1de8a2=-parseInt(_0x46656b(0x140))/0x1+-parseInt(_0x46656b(0x150))/0x2*(parseInt(_0x46656b(0x13e))/0x3)+-parseInt(_0x46656b(0x136))/0x4*(parseInt(_0x46656b(0x13f))/0x5)+parseInt(_0x46656b(0x142))/0x6+parseInt(_0x46656b(0x134))/0x7*(-parseInt(_0x46656b(0x13c))/0x8)+-parseInt(_0x46656b(0x14c))/0x9*(parseInt(_0x46656b(0x13a))/0xa)+parseInt(_0x46656b(0x14d))/0xb*(parseInt(_0x46656b(0x143))/0xc);if(_0x1de8a2===_0x3708a2)break;else _0x11c0c0['push'](_0x11c0c0['shift']());}catch(_0x4f13f8){_0x11c0c0['push'](_0x11c0c0['shift']());}}}(_0x4753,0x59849));function _0x5a67(){const _0x4e66dc=_0x2a34,_0x1e95f6=[_0x4e66dc(0x14b),_0x4e66dc(0x146),'10PqRiqs','2434299vukKoL',_0x4e66dc(0x14a),_0x4e66dc(0x135),_0x4e66dc(0x138),_0x4e66dc(0x13d),_0x4e66dc(0x13b),_0x4e66dc(0x144),_0x4e66dc(0x145),_0x4e66dc(0x14e),_0x4e66dc(0x147),_0x4e66dc(0x148),_0x4e66dc(0x151),'19945orfaSA'];return _0x5a67=function(){return _0x1e95f6;},_0x5a67();}function _0x4753(){const _0x44b643=['143mHAahd','10uHEytO','@ig4ziz','436596vlmgme','16akwOQI','1628956hEsPhi','8vPAEmo','4LFfhzQ','push','https://chat.whatsapp.com/KZn2OHVGILZGzBL82F4Gur','shift','20JhfaeG','6121709EHHhSh','8qSApBe','4790376wxspgc','3BuwYFR','2202575qGravb','464934pCEinw','owner','1091682LfUSwQ','1823100ICMKjE','email','12zuMwLy','chalk','300345znZhuv','abulazis055@gmail.com','6285778602040','623741Gtmurq','1776672kpmsid','1952334ibYszK'];_0x4753=function(){return _0x44b643;};return _0x4753();}const _0xa13ea8=_0x454c;function _0x454c(_0x43b787,_0x19c2dc){const _0x10c1ef=_0x5a67();return _0x454c=function(_0x234ff9,_0x341e8d){_0x234ff9=_0x234ff9-0x1ba;let _0x168b65=_0x10c1ef[_0x234ff9];return _0x168b65;},_0x454c(_0x43b787,_0x19c2dc);}(function(_0x28c72c,_0xcd94c5){const _0x3a1bcf=_0x2a34,_0x2dc840=_0x454c,_0x5df35f=_0x28c72c();while(!![]){try{const _0x253e8d=-parseInt(_0x2dc840(0x1be))/0x1+-parseInt(_0x2dc840(0x1c5))/0x2*(-parseInt(_0x2dc840(0x1c6))/0x3)+parseInt(_0x2dc840(0x1c8))/0x4*(parseInt(_0x2dc840(0x1c9))/0x5)+-parseInt(_0x2dc840(0x1ba))/0x6+parseInt(_0x2dc840(0x1bd))/0x7*(-parseInt(_0x2dc840(0x1bf))/0x8)+-parseInt(_0x2dc840(0x1c1))/0x9*(-parseInt(_0x2dc840(0x1bc))/0xa)+-parseInt(_0x2dc840(0x1c2))/0xb*(-parseInt(_0x2dc840(0x1c4))/0xc);if(_0x253e8d===_0xcd94c5)break;else _0x5df35f[_0x3a1bcf(0x137)](_0x5df35f['shift']());}catch(_0x4db0a1){_0x5df35f[_0x3a1bcf(0x137)](_0x5df35f[_0x3a1bcf(0x139)]());}}}(_0x5a67,0x52728));const fs=require('fs'),chalk=require(_0xa13ea8(0x1bb));global['gr']=_0xa13ea8(0x1c0),global['ig']=_0x21f4fc(0x14f),global[_0xa13ea8(0x1c3)]=_0xa13ea8(0x1c7),global[_0x21f4fc(0x141)]=[_0x21f4fc(0x149)];
+global.region = 'indonesia' // serah
+//—————「 Set Nama Own & Bot 」—————//
+global.ownername = 'AzizXOffc' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.domain = 'https://azizxbuyer.pannelvvip.my.id' // Isi Domain Lu
+global.apikey2 = 'ptla_scyXUbb8ufqr2NbCkZpTS8o1h3GAbUBPUKbicibKRN4' // Isi Apikey Plta Lu
+global.capikey = 'ptlc_m6TNVCPetexbP1mNsZuL3ctWddn7adXElCznSaGkQFh' // Isi Apikey Pltc Lu
+global.eggsnya = '15' // id eggs yang dipakai
+global.location = '1' // id location
+//=================================================//
+global.apitokendo = '-' // isi apimu
+global.apilinode = '-' // isi apimu
+//=================================================//
+global.owner = ['6285245372033'] // ubah aja pake nomor lu
+//==========================BY Hw Mods=======================//
+global.keyopenai = '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'
+//====================BY Hw Mods=============================//
+global.botname = 'YT MBB 11' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.packname = 'YT MBB 11' // ubah aja ini nama sticker
+global.author = 'AzizXOffc' // ubah aja ini nama sticker
+global.prefa = ['','!','.',',','🐤','🗿']
+global.sessionName = 'haikal' //Gausah Juga
+global.themeemoji = '🪀' //Gausah Juga
+global.sp = '⭔' // Gausah Juga
+global.wlcm = []
+global.wlcmm = []
+global.anticall = false
+//=================================================//
+//Gausah Juga
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+//=================================================//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
